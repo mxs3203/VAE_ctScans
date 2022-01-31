86b4de47ce052b6aade058b0f1addf3bb9c04fca
mxs3203 <mxs3203@g.rit.edu>
2022-01-31 20:23:11 +0100

    okay, leaky sucks! Added linear before variational latent

--- VAE_CT/VAEModel.py
+++ VAE_CT/VAEModel.py
@@ -15,50 +15,53 @@ class UnFlatten(nn.Module):
 
 
 class ConvVAE(nn.Module):
-    def __init__(self, image_channels=60, h_dim=256, z_dim=512, leakyalpha = 0.01):
+    def __init__(self, image_channels=60, h_dim=256, z_dim=512):
         super(ConvVAE, self).__init__()
         self.encoder = nn.Sequential(
             # 512/4=128, (B, 64, 128, 128)
             nn.Conv2d(image_channels, 64, kernel_size=4, stride=4),
-            nn.LeakyReLU(leakyalpha),
+            nn.ReLU(),
             # 128/4=32 (B, 80, 32, 32)
             nn.Conv2d(64, 80, kernel_size=4,stride=4),
-            nn.LeakyReLU(leakyalpha),nn.BatchNorm2d(80),
+            nn.ReLU(),
+            nn.BatchNorm2d(80),
             # 32/4=8 (B, 100, 8, 8)
             nn.Conv2d(80, 100, kernel_size=4,stride=4),
-            nn.LeakyReLU(leakyalpha),nn.BatchNorm2d(100),
+            nn.ReLU(),
+            nn.BatchNorm2d(100),
             # 8/8=1 (B, 120, 1, 1)
             nn.Conv2d(100, h_dim, kernel_size=8,stride=8),
-            nn.LeakyReLU(leakyalpha),nn.BatchNorm2d(h_dim),
-            nn.Dropout2d(0.1),
-            nn.Flatten()
+            nn.ReLU(),nn.BatchNorm2d(h_dim),
+            #nn.Dropout2d(0.1),
+            nn.Flatten(),
+            nn.Linear(h_dim, h_dim)
         )
 
         self.z_mean = nn.Linear(h_dim, z_dim)
         self.z_log_var = nn.Linear(h_dim, z_dim)
 
         self.decoder = nn.Sequential(
-            #nn.Linear(z_dim, h_dim), nn.ReLU(),
+            nn.Linear(z_dim, z_dim), nn.ReLU(),
             UnFlatten(-1, z_dim, 1, 1),
             nn.ConvTranspose2d(z_dim, 100, kernel_size=8,stride=8),
-            nn.LeakyReLU(leakyalpha),
+            nn.ReLU(),
             nn.ConvTranspose2d(100, 80, kernel_size=4,stride=4),
-            nn.LeakyReLU(leakyalpha),
+            nn.ReLU(),
             nn.ConvTranspose2d(80, 64, kernel_size=4,stride=4),
-            nn.LeakyReLU(leakyalpha),
+            nn.ReLU(),
             nn.ConvTranspose2d(64, image_channels, kernel_size=4, stride=4 ),
             nn.Sigmoid(),
         )
 
-    def reparameterize(self, mu, logvar):
+    def reparameterize(self, mu):
         eps = torch.randn(mu.size(0), mu.size(1)).to(mu.get_device())
-        z = mu + eps * torch.exp(logvar/0.5)
+        z = mu + eps * torch.randn_like(mu)
         return z
 
     def encode_img(self, x):
         x = self.encoder(x)
         z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
-        z = self.reparameterize(z_mean, z_log_var)
+        z = self.reparameterize(z_mean)
         return x,z, z_mean, z_log_var
 
     def forward(self, x):

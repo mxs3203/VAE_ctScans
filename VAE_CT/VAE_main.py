c5b7cb9670e1f24cd444a617b133d2610ae3a552
mxs3203 <mxs3203@g.rit.edu>
2022-02-01 16:01:21 +0100

    Tring MMD-VAE, I changed the loss function to produce better quality image. everything else is the same. Added visalization for every 10th epoch

--- VAE_CT/VAE_main.py
+++ VAE_CT/VAE_main.py
@@ -26,9 +26,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 writer = SummaryWriter(flush_secs=1)
 
 # set the learning parameters
-lr = 0.001
-epochs = 200
-batch_size = 20
+lr = 0.0001
+epochs = 300
+batch_size = 12
 model = ConvVAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 criterion = torch.nn.BCELoss(reduction='sum')
@@ -48,6 +48,23 @@ writer.add_text("Hyperparams",
                 "LR={}, batchSize={}".format(lr, batch_size))
 writer.add_text("Model", str(model.__dict__['_modules']))
 
+def gaussian_kernel(a, b):
+    dim1_1, dim1_2 = a.shape[0], b.shape[0]
+    depth = a.shape[1]
+    a = a.view(dim1_1, 1, depth)
+    b = b.view(1, dim1_2, depth)
+    a_core = a.expand(dim1_1, dim1_2, depth)
+    b_core = b.expand(dim1_1, dim1_2, depth)
+    numerator = (a_core - b_core).pow(2).mean(2)/depth
+    return torch.exp(-numerator)
+
+def MMD(a, b):
+    return gaussian_kernel(a, a).mean() + gaussian_kernel(b, b).mean() - 2*gaussian_kernel(a, b).mean()
+
+def loss_function(pred, true, latent):
+    return (pred-true).pow(2).mean(), MMD(torch.randn(200, 512, requires_grad = False).to(device), latent)
+
+
 def final_loss(bce_loss, mu, logvar):
     BCE = bce_loss
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
@@ -61,9 +78,10 @@ def train(ep):
         images = images.to(device)
         optimizer.zero_grad()
         recon_images, mu, logvar = model(images)
-
-        loss = criterion(recon_images, images)
-        loss,kld = final_loss(loss,mu, logvar)
+        loss, kld = loss_function(recon_images, images, mu)
+        loss = loss + kld
+        #loss = criterion(recon_images, images)
+        #loss,kld = final_loss(loss,mu, logvar)
         loss.backward()
         optimizer.step()
         running_loss.append(loss.cpu().detach())
@@ -87,8 +105,10 @@ def validate(ep):
         for images in valLoader:
             images = images.to(device)
             recon_images, mu, logvar = model(images)
-            loss = criterion(recon_images, images)
-            loss, kld = final_loss(loss, mu, logvar)
+            loss, kld = loss_function(recon_images, images, mu)
+            loss = loss + kld
+            #loss = criterion(recon_images, images)
+            #loss, kld = final_loss(loss, mu, logvar)
 
             running_loss.append(loss.cpu().detach())
             running_kld.append(kld.cpu().detach())
@@ -114,7 +134,7 @@ def plot_latent(ep):
     plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'])
     plt.title("TSNE of Validation DF latent vectors")
     #writer.add_figure('Valid/tsne', plt, ep)
-    plt.savefig("tsne.png")
+    plt.savefig("Res/tsne{}.png".format(ep))
     plt.show()
 
 
@@ -128,7 +148,7 @@ def visualize_recon(randomimg, ep):
         ax.imshow(im, cmap='gray')
     plt.tight_layout()
     #writer.add_figure('Valid/reconstruction', plt.show(), ep)
-    plt.savefig("reconstructions.png")
+    plt.savefig("Res/reconstructions{}.png".format(ep))
     plt.show()
 
 loss = []
@@ -136,9 +156,10 @@ for epoch in range(epochs):
     train_loss = train(epoch)
     loss.append(train_loss)
     recon_image = validate(epoch)
-    visualize_recon(recon_image.cpu().detach(), epoch)
-    plot_latent(epoch)
-    torch.save(model.state_dict(), "../saved_models/vae_model_ep_{}.pt".format(epoch))
+    if epoch % 10 == 1:
+        visualize_recon(recon_image.cpu().detach(), epoch)
+        plot_latent(epoch)
+        torch.save(model.state_dict(), "../saved_models/vae_model_ep_{}.pt".format(epoch))
 
 
 plt.plot(range(epochs), loss)

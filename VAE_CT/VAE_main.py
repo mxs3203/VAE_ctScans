5c3aa2c8a903d4a1ae7c3dcdcb84ee3c52b7b8d8
mateo <mxs3203@g.rit.edu>
2022-02-08 10:44:22 -0500

    failed attemped of making my own dicom to voxel merger. I run the old one on LUSC,LUAD and KIRC data

--- VAE_CT/VAE_main.py
+++ VAE_CT/VAE_main.py
@@ -1,4 +1,3 @@
-
 import pandas
 import torch
 import numpy as np
@@ -20,7 +19,6 @@ tsne = TSNE(n_components=2, verbose=0, random_state=123)
 transform = transforms.Compose([
     transforms.ToTensor()
 ])
-
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 writer = SummaryWriter(flush_secs=1)
@@ -49,6 +47,7 @@ writer.add_text("Hyperparams",
                 "LR={}, batchSize={}".format(lr, batch_size))
 writer.add_text("Model", str(model.__dict__['_modules']))
 
+
 def gaussian_kernel(a, b):
     dim1_1, dim1_2 = a.shape[0], b.shape[0]
     depth = a.shape[1]
@@ -56,20 +55,23 @@ def gaussian_kernel(a, b):
     b = b.view(1, dim1_2, depth)
     a_core = a.expand(dim1_1, dim1_2, depth)
     b_core = b.expand(dim1_1, dim1_2, depth)
-    numerator = (a_core - b_core).pow(2).mean(2)/depth
+    numerator = (a_core - b_core).pow(2).mean(2) / depth
     return torch.exp(-numerator)
 
+
 def MMD(a, b):
-    return gaussian_kernel(a, a).mean() + gaussian_kernel(b, b).mean() - 2*gaussian_kernel(a, b).mean()
+    return gaussian_kernel(a, a).mean() + gaussian_kernel(b, b).mean() - 2 * gaussian_kernel(a, b).mean()
+
 
 def loss_function(pred, true, latent):
-    return (pred-true).pow(2).mean(), MMD(torch.randn(512, L, requires_grad = False).to(device), latent)
+    return (pred - true).pow(2).mean(), MMD(torch.randn(512, L, requires_grad=False).to(device), latent)
 
 
 def final_loss(bce_loss, mu, logvar):
     BCE = bce_loss
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE + KLD, KLD
+
 
 def train(ep):
     model.train()
@@ -81,21 +83,22 @@ def train(ep):
         recon_images, mu, logvar = model(images)
         loss, kld = loss_function(recon_images, images, mu)
         loss = loss + kld
-        #loss = criterion(recon_images, images)
-        #loss,kld = final_loss(loss,mu, logvar)
+        # loss = criterion(recon_images, images)
+        # loss,kld = final_loss(loss,mu, logvar)
         loss.backward()
         optimizer.step()
         running_loss.append(loss.cpu().detach())
         running_kld.append(kld.cpu().detach())
 
     to_print = "Epoch[{}/{}] \n\tTraining Loss: {:.8f} KLD: {:.8f}".format(epoch + 1,
-                                                               epochs,
-                                                               np.mean(running_loss),
-                                                               np.mean(running_kld))
+                                                                           epochs,
+                                                                           np.mean(running_loss),
+                                                                           np.mean(running_kld))
     print(to_print)
     writer.add_scalar('Loss/Train', np.mean(running_loss), ep)
     writer.add_scalar('KLD/Train', np.mean(running_kld), ep)
     return np.mean(running_loss)
+
 
 def validate(ep):
     # Validation
@@ -108,18 +111,19 @@ def validate(ep):
             recon_images, mu, logvar = model(images)
             loss, kld = loss_function(recon_images, images, mu)
             loss = loss + kld
-            #loss = criterion(recon_images, images)
-            #loss, kld = final_loss(loss, mu, logvar)
+            # loss = criterion(recon_images, images)
+            # loss, kld = final_loss(loss, mu, logvar)
 
             running_loss.append(loss.cpu().detach())
             running_kld.append(kld.cpu().detach())
         to_print = "\tValidation Loss: {:.8f} KLD: {:.8f}".format(
-                                                       np.mean(running_loss),
-                                                       np.mean(running_kld))
+            np.mean(running_loss),
+            np.mean(running_kld))
         print(to_print)
         writer.add_scalar('Loss/Valid', np.mean(running_loss), ep)
         writer.add_scalar('KLD/Valid', np.mean(running_kld), ep)
     return recon_images[0, :, :, :]
+
 
 def plot_latent(ep):
     print("\tCalculating TSNE of validation data latent vectors")
@@ -128,13 +132,13 @@ def plot_latent(ep):
         x, z, z_mean, z_log_var = model.encode_img(x.to(device))
         z = z.to('cpu').detach().numpy()
         z = pandas.DataFrame(z)
-        total = pandas.concat([total ,z], ignore_index=True)
+        total = pandas.concat([total, z], ignore_index=True)
 
     tsne_results = tsne.fit_transform(total)
     tsne_results = pandas.DataFrame(tsne_results, columns=['tsne1', 'tsne2'])
     plt.scatter(tsne_results['tsne1'], tsne_results['tsne2'])
     plt.title("TSNE of Validation DF latent vectors")
-    #writer.add_figure('Valid/tsne', plt, ep)
+    # writer.add_figure('Valid/tsne', plt, ep)
     plt.savefig("Res/tsne{}.png".format(ep))
     plt.show()
 
@@ -148,9 +152,10 @@ def visualize_recon(randomimg, ep):
         im = np.array(randomimg[i, :, :])
         ax.imshow(im, cmap='gray')
     plt.tight_layout()
-    #writer.add_figure('Valid/reconstruction', plt.show(), ep)
+    # writer.add_figure('Valid/reconstruction', plt.show(), ep)
     plt.savefig("Res/reconstructions{}.png".format(ep))
     plt.show()
+
 
 loss = []
 for epoch in range(epochs):
@@ -166,6 +171,5 @@ torch.save(model.state_dict(), "../saved_models/vae_model_last.pt")
 plt.plot(range(epochs), loss)
 plt.title("Total Loss")
 plt.show()
-
 
 print('TRAINING COMPLETE')
--- dicom_to_voxel/dicom_to_voxel.py
+++ dicom_to_voxel/dicom_to_voxel.py
@@ -16,63 +16,67 @@ parser.add_argument("output_image", nargs="?")
 parser.add_argument("series_name", nargs="?")
 args = parser.parse_args()
 
-inital_path = "/data/raw/"
+inital_path = "/media/mateo/data1/KIRC_CT/manifest-1644254639955/TCGA-KIRC/"
 all_folders = glob.glob("{}*".format(inital_path))
 for folder in all_folders:
-    f = glob.glob("{}/*".format(folder))
-    if len(f):
-        print(f[0])
-        # current directory by default
-        dirName = "{}/".format(f[0])
-        if args.dicom_directory:
-            dirName = args.dicom_directory
+    ff = glob.glob("{}/*".format(folder))
+    for f in ff:
+        f = glob.glob("{}/*".format(f))
+        if len(f):
+            print(f[0])
+            # current directory by default
+            dirName = "{}/".format(f[0])
+            if args.dicom_directory:
+                dirName = args.dicom_directory
 
-        PixelType = itk.ctype("signed short")
-        Dimension = 3
+            PixelType = itk.ctype("signed short")
+            Dimension = 3
 
-        ImageType = itk.Image[PixelType, Dimension]
+            ImageType = itk.Image[PixelType, Dimension]
 
-        namesGenerator = itk.GDCMSeriesFileNames.New()
-        namesGenerator.SetUseSeriesDetails(True)
-        namesGenerator.AddSeriesRestriction("0008|0021")
-        namesGenerator.SetGlobalWarningDisplay(False)
-        namesGenerator.SetDirectory(dirName)
+            namesGenerator = itk.GDCMSeriesFileNames.New()
+            namesGenerator.SetUseSeriesDetails(True)
+            namesGenerator.AddSeriesRestriction("0008|0021")
+            namesGenerator.SetGlobalWarningDisplay(False)
+            namesGenerator.SetDirectory(dirName)
 
-        seriesUID = namesGenerator.GetSeriesUIDs()
+            seriesUID = namesGenerator.GetSeriesUIDs()
 
-        if len(seriesUID) < 1:
-            print("No DICOMs in: " + dirName)
+            if len(seriesUID) < 1:
+                print("No DICOMs in: " + dirName)
 
 
-        print("The directory: " + dirName)
-        print("Contains the following DICOM Series: ")
-        for uid in seriesUID:
-            print(uid)
+            print("The directory: " + dirName)
+            print("Contains the following DICOM Series: ")
+            for uid in seriesUID:
+                print(uid)
 
-        seriesFound = False
-        for uid in seriesUID:
-            seriesIdentifier = uid
-            if args.series_name:
-                seriesIdentifier = args.series_name
-                seriesFound = True
-            print("Reading: " + seriesIdentifier)
-            fileNames = namesGenerator.GetFileNames(seriesIdentifier)
+            seriesFound = False
+            for uid in seriesUID:
+                seriesIdentifier = uid
+                if args.series_name:
+                    seriesIdentifier = args.series_name
+                    seriesFound = True
+                print("Reading: " + seriesIdentifier)
+                fileNames = namesGenerator.GetFileNames(seriesIdentifier)
 
-            reader = itk.ImageSeriesReader[ImageType].New()
-            dicomIO = itk.GDCMImageIO.New()
-            reader.SetImageIO(dicomIO)
-            reader.SetFileNames(fileNames)
-            reader.ForceOrthogonalDirectionOff()
+                reader = itk.ImageSeriesReader[ImageType].New()
+                dicomIO = itk.GDCMImageIO.New()
+                reader.SetImageIO(dicomIO)
+                reader.SetFileNames(fileNames)
+                reader.ForceOrthogonalDirectionOff()
 
-            writer = itk.ImageFileWriter[ImageType].New()
-            outFileName = os.path.join(dirName, seriesIdentifier + ".nrrd")
-            if args.output_image:
-                outFileName = args.output_image
-            writer.SetFileName(outFileName)
-            writer.UseCompressionOn()
-            writer.SetInput(reader.GetOutput())
-            print("Writing: " + outFileName)
-            writer.Update()
+                writer = itk.ImageFileWriter[ImageType].New()
+                outFileName = os.path.join(dirName, folder,seriesIdentifier + ".nrrd")
+                if args.output_image:
+                    outFileName = args.output_image
+                writer.SetFileName(outFileName)
+                writer.UseCompressionOn()
+                writer.SetInput(reader.GetOutput())
+                print("Writing: " + outFileName)
+                writer.Update()
 
-            if seriesFound:
-                print("Nothing foud... moving on")
+                if seriesFound:
+                    print("Nothing foud... moving on")
+
+# find TCGA-LUSC/ -name "*.nrrd" -exec mv "{}" LUSC-NRRD/ \;
